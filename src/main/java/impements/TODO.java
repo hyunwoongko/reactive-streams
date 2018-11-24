b2cb6f9b7d07242af2b76b4b65d7ca114016f489
gusdnd852 <gusdnd852@naver.com>
2018-11-24 15:33:32 +0900

    V 1.0 : 멀티쓰레드에서 onNext 여러번호출시 complete 안됨, 배압 미구현

--- src/main/java/impements/TODO.java
+++ src/main/java/impements/TODO.java
@@ -6,6 +6,5 @@ package impements;
  * @Homepage : https://github.com/gusdnd852
  */
 public class TODO {
-    //TODO 1 : 멀티쓰레드 환경에서 같은 메소드 두번 호출시 onComplete 안됨.
-    //TODO 2 : backPressure 흐름 막는건 되는데 다음 값으로 갱신 안됨.
+    //TODO 1 : 멀티쓰레드 환경에서 같은 메소드 여러번 호출시 onComplete 안됨.
 }
--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,10 +1,28 @@
 package test;
 
+import impements.Executors;
+import impements.publisher.Flux;
+import impements.subscriber.With;
+
+import java.util.Arrays;
+
 /**
  * @Author : Hyunwoong
  * @When : 2018-11-24 오전 6:57
  * @Homepage : https://github.com/gusdnd852
  */
 public class Main {
+    public static void main(String[] args) {
+        With subscriber = new With()
+                .complete(() -> System.out.println("컴플릿"));
 
+        Flux.empty()
+                .fork(Executors::backgroundThread)
+                .next(System.out::println)
+                .next(System.out::println)
+                .join()
+                .subscribe(subscriber);
+
+        subscriber.onNext(Arrays.asList(1, 2, 3, 4, 5, 6));
+    }
 }
